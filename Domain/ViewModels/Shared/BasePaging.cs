06db6d985152ebe6187bdfe2eeeddf0b5a500c23
Amirgh8080 <ghasemi.amir8080@gmail.com>
2022-10-29 02:31:10 +0330

    Fix Bugs

--- Domain/ViewModels/Shared/BasePaging.cs
+++ Domain/ViewModels/Shared/BasePaging.cs
@@ -12,7 +12,7 @@ namespace Domain.ViewModels.Shared
         public BasePaging()
         {
             Page = 1;
-            TakeEntity = 2;
+            TakeEntity = 9;
             HowManyShowPageAfterAndBefore = 5;
             Entities = new List<T>();
         }
