316e5b61f62591010d9c33162629d1d4d39e9610
Mohammad Hosein Soleymanpour <111628438+Mohammadsoleymanpour@users.noreply.github.com>
2022-10-31 19:05:51 +0330

    log ViewModel

---
+++ Domain/ViewModels/Log/FilterUserLogViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models.Common;
+using Domain.ViewModels.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ViewModels.Log
+{
+    public class FilterUserLogViewModel:BasePaging<Models.Common.Log>
+    {
+        public string? UserName { get; set; }
+        public string? Activity { get; set; }
+        
+    }
+}
