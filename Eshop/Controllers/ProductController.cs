19afa3a4d9898be1efd3c7392f30888c9e03695c
Mohammad Hosein Soleymanpour <111628438+Mohammadsoleymanpour@users.noreply.github.com>
2022-11-02 20:14:51 +0330

    check MOBILE

--- Eshop/Controllers/ProductController.cs
+++ Eshop/Controllers/ProductController.cs
@@ -110,8 +110,8 @@ namespace Eshop.Controllers
         [Route("AllProducts")]
         public async Task<IActionResult> AllProducts(FilterProductByCategory filter)
         {
-            var products =await _productService.GetAllProductsCategoryAsync();
-            return View("ProductFilter");
+            var products =await _productService.GetAllProducts(filter);
+            return View("ProductNonFilter",products);
         }
         [Route("Products")]
         public async Task<IActionResult> Products(FilterProductByCategory filter)
